70349aea87da5346f99cfa39d74a3e590269aa0d
Agusx1211 <agusgit@pm.me>
2025-08-01 16:08:51 +0000

    Initial catapult migration

--- scripts/factories/v1/index.ts
+++
@@ -1,8 +0,0 @@
-import { FactoryV1 } from './FactoryV1'
-import { GuestModuleV1 } from './GuestModuleV1'
-import { MainModuleUpgradableV1 } from './MainModuleUpgradableV1'
-import { MainModuleV1 } from './MainModuleV1'
-import { RequireFreshSignerV1 } from './RequireFreshSignerV1'
-import { SequenceUtilsV1 } from './SequenceUtilsV1'
-
-export { FactoryV1, MainModuleV1, MainModuleUpgradableV1, GuestModuleV1, SequenceUtilsV1, RequireFreshSignerV1 }
--- scripts/factories/v2/index.ts
+++
@@ -1,8 +0,0 @@
-import { FactoryV2 } from './FactoryV2'
-import { GuestModuleV2 } from './GuestModuleV2'
-import { MainModuleUpgradableV2 } from './MainModuleUpgradableV2'
-import { MainModuleV2 } from './MainModuleV2'
-import { SequenceUtilsV2 } from './SequenceUtilsV2'
-import { TrustFactory } from './commons/TrustFactory'
-
-export { FactoryV2, MainModuleV2, MainModuleUpgradableV2, GuestModuleV2, SequenceUtilsV2, TrustFactory }
